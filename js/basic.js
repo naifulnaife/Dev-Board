cc531bd32e6db0a801539388e5abce15ad92134c
naiful.naife <naifulnaife009@gmail.com>
2025-03-01 19:03:32 +0600

    activity log part

--- js/basic.js
+++ js/basic.js
@@ -1,12 +1,5 @@
-//hand-cursor
-document.getElementById('discover').addEventListener("click", function(event){
-    window.location.href="./main.html"
-})
 
-
-document.getElementById('back-to-desk-btn').addEventListener("click", function(event) {
-        window.location.href ="./index.html"
-    })
-    cursorPointer("discover");
 
   
+
+
--- js/utilities.js
+++ js/utilities.js
@@ -1,34 +1,88 @@
+//couting task
 function countingTask(event){
+        
+        
+        
     
         let taskNumber = document.getElementById("task-num");
         let taskCount = document.getElementById("task-count");
         let convertedNumber = parseInt(taskNumber.innerText);
         let convertedCount = parseInt(taskCount.innerText);
+
+        alert("Board updated successfully");
+        if (convertedNumber === 1) { 
+            alert("Congrats!!!You have completed all the current tasks");
+        }
         
         if(!isNaN(convertedNumber) && !isNaN(convertedCount)){
             taskNumber.innerText = convertedNumber - 1;
             taskCount.innerText = convertedCount + 1;
         }
+
+        //Activity-Log section
+
+        
+        let newdiv = document.createElement("div");
+
+        newdiv.innerHTML = `
+            <div class="text-[10px] bg-[#F4F7FF] p-2 rounded-lg my-2">
+            You have completed the task ${new Date().toLocaleString()}
+        </div>
+        `;
+
+
+        let parentElement = document.getElementById("task-section");
+
+        parentElement.appendChild(newdiv);
+    
+
+        //button desabled
         let btn = event.target;
         btn.disabled = true;
         btn.style.backgroundColor = '#A1B9FF';
         btn.style.cursor = 'not-allowed';
-}
+    }
+
+
 
         document.querySelectorAll(".btn-task").forEach(button => {
             button.addEventListener("click",countingTask);
         });
 
-        const colors = ["#FF5733", "#33FF57", "#3357FF", "#FFFF33", "#FF33FF", "#33FFFF", "#F1C40F", "#9B59B6"];
+     //bg-color changing
+
+const colors = ["#FF5733", "#33FF57", "#3357FF", "#FFFF33", "#FF33FF", "#33FFFF", "#F1C40F", "#9B59B6"];
 
        
-        function changeBackgroundColor(){
-            
-            const randomColor=colors[Math.floor(Math.random()*colors.length)];
+function changeBackgroundColor(){
+    
+    const randomColor=colors[Math.floor(Math.random()*colors.length)];
 
-            
-            document.body.style.backgroundColor = randomColor;
-        }
+    
+    document.body.style.backgroundColor = randomColor;
+}
 
-        document.getElementById("colorIcon").addEventListener("click", changeBackgroundColor);
+document.getElementById("colorIcon").addEventListener("click", changeBackgroundColor);   
         
+
+
+//update date-time
+
+function updateDateTime() {
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    
+    const currentDate = new Date();
+    const day = days[currentDate.getDay()];
+    const month = months[currentDate.getMonth()];
+    const date = currentDate.getDate();
+    const year = currentDate.getFullYear();
+
+    const formattedDate = `${day} ,<br><strong>${month} ${date} ${year}</strong>`;
+    document.getElementById("datetime").innerHTML = formattedDate;
+}
+updateDateTime();
+
+
+
+
